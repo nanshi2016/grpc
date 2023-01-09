db33e5b165b58392ae94cfc896226e218f904092
Hannah Shi <hannahshisfb@gmail.com>
2023-01-09 09:07:06 -0800

    cf event engine client support

--- test/core/event_engine/test_suite/cf_event_engine_test.cc
+++ test/core/event_engine/test_suite/cf_event_engine_test.cc
@@ -19,15 +19,22 @@
 
 #include "src/core/lib/event_engine/cf_engine/cf_engine.h"
 #include "test/core/event_engine/test_suite/event_engine_test.h"
+#include "test/core/event_engine/test_suite/oracle_event_engine_posix.h"
 #include "test/core/util/test_config.h"
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   grpc::testing::TestEnvironment env(&argc, argv);
+  gpr_set_log_verbosity(GPR_LOG_SEVERITY_DEBUG);
+
   auto factory = []() {
     return std::make_unique<grpc_event_engine::experimental::CFEventEngine>();
   };
-  SetEventEngineFactories(factory, factory);
+  auto oracle_factory = []() {
+    return std::make_unique<
+        grpc_event_engine::experimental::PosixOracleEventEngine>();
+  };
+  SetEventEngineFactories(factory, oracle_factory);
   // TODO(ctiller): EventEngine temporarily needs grpc to be initialized first
   // until we clear out the iomgr shutdown code.
   grpc_init();
